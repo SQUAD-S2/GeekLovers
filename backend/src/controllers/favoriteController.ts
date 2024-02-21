bf356a0b2fc944c5a6d3f904921e4e8c90f0f185
GbAraujoSouzaWin <gabriel.souzaaraujo109@gmail.com>
2024-02-21 18:28:28 -0300

    remove: remover funcionalidade de cookies

--- backend/src/controllers/favoriteController.ts
+++ backend/src/controllers/favoriteController.ts
@@ -1,27 +1,27 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 import Utils from './utils';
+import Auth from '../config/auth';
 
 const prisma = new PrismaClient();
 
 class FavoriteController {
   async create(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
-        let favoritesInput: Prisma.FavoritesCreateInput = {
-          user: { connect: { id: userId } },
-        };
-
-        const newFavorites = await prisma.favorites.create({
-          data: favoritesInput,
-        });
-        return response.status(201).json(newFavorites);
-      } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
       }
+      const userId = payload.sub;
+      let favoritesInput: Prisma.FavoritesCreateInput = {
+        user: { connect: { id: userId } },
+      };
+
+      const newFavorites = await prisma.favorites.create({
+        data: favoritesInput,
+      });
+      return response.status(201).json(newFavorites);
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
     }
@@ -29,32 +29,31 @@ class FavoriteController {
 
   async favoriteProduct(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
-        const productId = request.body.productId;
-
-        const product = await prisma.product.findUnique({
-          where: { id: productId },
-        });
-        if (!product) {
-          return response.status(404).json({ message: 'Produto não encontrado' });
-        }
-
-        const updatedFavorites = await prisma.favorites.update({
-          where: {
-            userId: userId,
-          },
-          data: {
-            products: { connect: { id: productId } },
-          },
-        });
-
-        return response.status(201).json(updatedFavorites);
-      } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
       }
+      const userId = payload.sub;
+      const productId = request.body.productId;
+
+      const product = await prisma.product.findUnique({
+        where: { id: productId },
+      });
+      if (!product) {
+        return response.status(404).json({ message: 'Produto não encontrado' });
+      }
+
+      const updatedFavorites = await prisma.favorites.update({
+        where: {
+          userId: userId,
+        },
+        data: {
+          products: { connect: { id: productId } },
+        },
+      });
+
+      return response.status(201).json(updatedFavorites);
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
     }
@@ -62,32 +61,31 @@ class FavoriteController {
 
   async unFavoriteProduct(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
-        const productId = request.body.productId;
-
-        const product = await prisma.product.findUnique({
-          where: { id: productId },
-        });
-        if (!product) {
-          return response.status(404).json({ message: 'Produto não encontrado' });
-        }
-
-        const updatedFavorites = await prisma.favorites.update({
-          where: {
-            userId: userId,
-          },
-          data: {
-            products: { disconnect: { id: productId } },
-          },
-        });
-
-        return response.status(201).json(updatedFavorites);
-      } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
       }
+      const userId = payload.sub;
+      const productId = request.body.productId;
+
+      const product = await prisma.product.findUnique({
+        where: { id: productId },
+      });
+      if (!product) {
+        return response.status(404).json({ message: 'Produto não encontrado' });
+      }
+
+      const updatedFavorites = await prisma.favorites.update({
+        where: {
+          userId: userId,
+        },
+        data: {
+          products: { disconnect: { id: productId } },
+        },
+      });
+
+      return response.status(201).json(updatedFavorites);
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
     }
@@ -95,20 +93,19 @@ class FavoriteController {
 
   async getFavorites(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
-
-        const favorites = await prisma.favorites.findUnique({
-          where: { userId: userId },
-          include: { products: true },
-        });
-
-        return response.status(201).json(favorites);
-      } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
       }
+      const userId = payload.sub;
+
+      const favorites = await prisma.favorites.findUnique({
+        where: { userId: userId },
+        include: { products: true },
+      });
+
+      return response.status(201).json(favorites);
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
     }
