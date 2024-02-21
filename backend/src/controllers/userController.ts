ed46588be49cfde90ece4a76cb05894fc781b85f
GbAraujoSouzaWin <gabriel.souzaaraujo109@gmail.com>
2024-02-21 19:03:57 -0300

    remove: remover funcionalidade de cookies

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,7 +1,6 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
 import Auth from '../config/auth';
-import Utils from './utils';
 
 const prisma = new PrismaClient();
 
@@ -55,10 +54,15 @@ class UserController {
 
   async index(request: Request, response: Response) {
     try {
-      const { id } = request.params;
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
+      }
+      const userId = Number(payload.sub);
       const user = await prisma.user.findUnique({
         where: {
-          id: Number(id),
+          id: Number(userId),
         },
       });
       return response.status(201).json(user);
@@ -69,31 +73,30 @@ class UserController {
 
   async update(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
-        const { email, name, profilePicture, status, card, address, phone, role } = request.body;
-        let userInput: Prisma.UserUpdateInput = {
-          email,
-          name,
-          profilePicture,
-          status,
-          card,
-          address,
-          phone,
-          role,
-        };
-
-        const user = await prisma.user.update({
-          where: { id: userId },
-          data: userInput,
-        });
-
-        return response.status(201).json(user);
-      } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
       }
+      const userId = Number(payload.sub);
+      const { email, name, profilePicture, status, card, address, phone, role } = request.body;
+      let userInput: Prisma.UserUpdateInput = {
+        email,
+        name,
+        profilePicture,
+        status,
+        card,
+        address,
+        phone,
+        role,
+      };
+
+      const user = await prisma.user.update({
+        where: { id: userId },
+        data: userInput,
+      });
+
+      return response.status(201).json(user);
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
     }
@@ -101,38 +104,38 @@ class UserController {
 
   async updatePassword(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
+      }
+      const userId = Number(payload.sub);
 
-        // encontrar usuário pelo id
-        const user = await prisma.user.findUnique({
+      // encontrar usuário pelo id
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        return response.status(404).json({ message: 'Usuário não encontrado.' });
+      }
+
+      // usuário precisa informar a senha atual para poder altera-la
+      const { newPassword, currentPassword } = request.body;
+      if (Auth.checkPassword(currentPassword, user.hash, user.salt)) {
+        const { hash, salt } = Auth.generatePassword(newPassword);
+
+        let userInput: Prisma.UserUpdateInput = {
+          hash: hash,
+          salt: salt,
+        };
+        const updatedUser = await prisma.user.update({
           where: { id: userId },
+          data: userInput,
         });
-
-        if (!user) {
-          return response.status(404).json({ message: 'Usuário não encontrado.' });
-        }
-
-        // usuário precisa informar a senha atual para poder altera-la
-        const { newPassword, currentPassword } = request.body;
-        if (Auth.checkPassword(currentPassword, user.hash, user.salt)) {
-          const { hash, salt } = Auth.generatePassword(newPassword);
-
-          let userInput: Prisma.UserUpdateInput = {
-            hash: hash,
-            salt: salt,
-          };
-          const updatedUser = await prisma.user.update({
-            where: { id: userId },
-            data: userInput,
-          });
-          return response.status(201).json(updatedUser);
-        } else {
-          return response.status(401).json({ message: 'Senha Atual incorreta.' });
-        }
+        return response.status(201).json(updatedUser);
       } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+        return response.status(401).json({ message: 'Senha Atual incorreta.' });
       }
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
@@ -141,19 +144,18 @@ class UserController {
 
   async destroy(request: Request, response: Response) {
     try {
-      const cookiesHeader = request.headers.cookie;
-
-      if (cookiesHeader) {
-        const userId = Utils.getIdFromCookies(cookiesHeader);
-        const user = await prisma.user.delete({
-          where: {
-            id: Number(userId),
-          },
-        });
-        return response.status(201).json(user);
-      } else {
-        return response.status(401).json({ message: 'Usuário não autenticado' });
+      const token = Auth.getToken(request);
+      const payload = Auth.decodeJWT(token);
+      if (!payload) {
+        return response.status(403).json({ message: 'Não autorizado' });
       }
+      const userId = Number(payload.sub);
+      const user = await prisma.user.delete({
+        where: {
+          id: Number(userId),
+        },
+      });
+      return response.status(201).json(user);
     } catch (error: any) {
       return response.status(500).json({ error: error.message });
     }
