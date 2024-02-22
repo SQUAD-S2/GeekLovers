5f9dd98b8fd8c459794f567478205188008c08cc
iwmillyx <emillyts@dcc.ufrj.br>
2024-02-21 23:20:58 -0300

    alterações tela_de_carrinho

--- frontend/src/pages/Tela_de_Carrinho/index.tsx
+++ frontend/src/pages/Tela_de_Carrinho/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { View } from "./style";
+import { View, Container, TitleText, ImagemPerfil, Text } from "./style";
 
-export default function Carrinho () {
-    return(
-        <View>
-
-
-
-
-
-        </View>
-    )
+export default function Carrinho() {
+  return (
+    <View>
+      <Container>
+        <TitleText>Carrinho</TitleText>
+        <ImagemPerfil source={require("../../../assets/Ellipse.png")} />
+      </Container>
+      <Text>3 itens</Text>
+    </View>
+  );
 }
