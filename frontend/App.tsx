c082fde81024bc81676ddb406152fb6337a5e762
lucasstefano <lucasstefanof@gmail.com>
2024-02-22 15:11:04 -0300

    new front

---
+++ frontend/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Registro from './src/pages/pagina-de-registro';
+import Login from './src/pages/tela-de-Login';
+
+export default function App() {
+    return (
+        <Registro/>
+    );
+}
+
+
