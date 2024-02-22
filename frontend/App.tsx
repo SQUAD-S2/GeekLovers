6bc7424a3ae85b2b14513c6f5dc254f7e0ff6dea
lucasstefano <lucasstefanof@gmail.com>
2024-02-22 16:04:54 -0300

    Feature: New Frontend

---
+++ frontend/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Favoritos from './src/pages/tela-de-favoritos/index';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Favoritos/>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
---
+++ frontend/src/components/ProdutoCard/style.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex-direction: column;
+    align-items: center;
+    background-color: #f3f3f3;
+    width: 38vw;
+    height: 20vh;
+    border-radius: 0.5rem;
+    border: 2px solid #F4D831;
+    padding: 0.5rem;
+    margin: 1rem 2rem;
+    box-shadow: 0px 4px 4px  #D9D9D9;
+`
+export const Container1 = styled.View`
+    justify-content: flex-start;
+    padding: 0.1rem 1.2rem 0.1rem 0;
+`
+
+export const Image = styled.Image`
+    width: 3.9rem;
+    height: 5.9rem;
+    margin: 0.5rem;
+    display: flex;
+    align-items: center;
+`
+
+export const Title = styled.Text`
+    font-size: 1rem;
+    margin: 0.3rem 0;
+`
+
+export const SubTitle = styled.Text`
+    font-size: 1rem;
+    font-weight: 650;
+`
