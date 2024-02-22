346c9f0b7c41164e158ad3cfe5d4841bcc0bb97e
lucasstefano <lucasstefanof@gmail.com>
2024-02-22 15:50:38 -0300

    init: novas paginas

---
+++ frontend/src/pages/register/style.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components/native';
+
+export const Title = styled.Text`
+    text-align: center;
+    font-size: 3.25rem;
+    padding: 1rem 0 4rem 5rem;
+`
+
+export const InputLabel = styled.Text`
+    text-align: left;
+    color: #988A3A;
+
+`
+export const Input = styled.TextInput`
+    color: #03030380;
+    max-width:80vw;
+    padding: 1.125rem 14.4375rem 1.125rem 1rem;
+    margin: 0.5rem 0;
+    border-radius: 0.3125rem;
+    border-width: 0.13rem;
+    border-style: solid;
+    border-color: #F3C63C;
+`
+
+export const TextTerms = styled.Text`
+    text-align: center;
+    color: red;
+`
+
+export const Button1 = styled.TouchableOpacity` 
+    padding: 1.4rem;
+    align-items: center; 
+    gap: 1rem; 
+    border-radius: 1rem;
+    background: #EA618A; 
+    margin-top: 1rem; 
+    margin-right: 1rem; 
+    width: 27rem; 
+    height: 4.5rem; 
+    box-shadow: 0.1rem 0.2rem  rgba(0, 0, 0, 0.25); 
+` 
+export const ButtonText1 = styled.Text` 
+    font-size: 1.25rem;
+    color: #F3F3F3; 
+` 
+
+export const Button2 = styled.TouchableOpacity`
+    padding: 1.6rem;
+    align-items: center; 
+    gap: 1rem; 
+    margin-top: 1rem; 
+    margin-right: 1rem; 
+    width: 27rem; 
+    height: 4.5rem; 
+` 
+
+export const ButtonText2 = styled.Text`
+    color: black; 
+    text-align: left; 
+`
+
+export const ButtonText3 = styled.Text`
+    color: #EA618A; 
+    text-align: left; 
+`
