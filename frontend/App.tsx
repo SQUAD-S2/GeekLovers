6e14225aa0b6108e2963acf54fc0bcb0794e3a3b
Bruna Nobrega <121074742+nobregaabruna@users.noreply.github.com>
2024-02-21 22:38:09 -0300

    feature: adição da tela de favoritos

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Favoritos from './src/pages/tela-de-favoritos/index';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <Favoritos/>
             <StatusBar style="auto" />
         </View>
     );
